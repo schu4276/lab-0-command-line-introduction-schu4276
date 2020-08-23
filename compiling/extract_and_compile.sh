bf0e4f17bda9cb2e349bc689b5621ce1b22afac4
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-23 16:03:08 -0500

    I have done the shell check, and then took the suggestions that the shellcheck offered. I added double quotes around variable names and added || exit to the cd commands incase the commands were to fail. When run, the script is currently outputing the correct output as per the lab write up. 'Prime 17 = 59'

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -9,9 +9,9 @@ Dir_name=$2
 tar -xf NthPrime.tgz -C $Dir_name
 # Going into the NthPrimeDirectory that the file was extracted to
 #and compiling the C program that was extracted, generating a executable called NthPrime
-cd
-cd $Dir_name/NthPrime
+cd || exit
+cd "$Dir_name"/NthPrime || exit
 gcc main.c nth_prime.c nth_prime.h -o NthPrime
-$Dir_name/NthPrime/NthPrime $NUMBER 
+"$Dir_name"/NthPrime/NthPrime "$NUMBER" 
 
 
