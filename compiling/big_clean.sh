d9edfe5eb2b9d8a309d5ef9808a9e4757f5ae0a0
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-23 16:50:14 -0500

    I created the big_clean.sh file and set variable names to the two inputs it is supposed to take in I also put the shebang into the script at the top

---
+++ compiling/big_clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+#the script takes in two inputs, the first the na,e of a compressed 
+#tar arhive that contains the files to be processed, 
+#The second is the name of a scratch directory 
+ARCHIVE_NAME=$1
+SCRATCH=$2
