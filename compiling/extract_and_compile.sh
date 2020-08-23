99e504f6780407856f3ddeb95772d76530235d68
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-23 00:38:06 -0500

    I have made an attempt to compile the NthPrime file.  I am generating errors, and having a hard time figuring out how to navigate to the proper areas in order to use the gcc command.  I am commiting even though the errors are present.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -7,4 +7,10 @@ Dir_name=$2
 # extracting the contents of the tar archive, and putting them into 
 # the directory specified by the user
 tar -xf NthPrime.tgz -C $Dir_name
+# Going into the NthPrimeDirectory that the file was extracted to
+#and compiling the C program that was extracted, generating a executable called NthPrime
+cd
+gcc $Dir_name/NthPrime -o NthPrime
+$Dir_name/NthPrime/NthPrime $NUMBER 
+
 
