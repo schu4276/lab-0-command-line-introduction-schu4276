d87f23c3ac7e1d0036592109fe298031b77acd89
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-25 09:48:42 -0500

    I have made changes to this script because the instructions from nic have changed. I got rid of the fact that it takes in two inputs, and changed it so the tar file is extracted into the current dir,

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,16 +2,14 @@
 #first script of lab 0
 ##### CONSTANTS	
 NUMBER=$1
-Dir_name=$2
 ##### MAIN
 # extracting the contents of the tar archive, and putting them into 
 # the directory specified by the user
-tar -zxf NthPrime.tgz -C $Dir_name
+tar -xzf NthPrime.tgz 
 # Going into the NthPrimeDirectory that the file was extracted to
 #and compiling the C program that was extracted, generating a executable called NthPrime
-cd || exit
-cd "$Dir_name"/NthPrime || exit
+cd NthPrime 
 gcc main.c nth_prime.c nth_prime.h -o NthPrime
-"$Dir_name"/NthPrime/NthPrime "$NUMBER" 
+./NthPrime "$NUMBER" 
 
 
